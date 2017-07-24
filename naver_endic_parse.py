067ee0d70617e6a7b4581b6a1661b572bef47dd9
SSELAB-129 <jungyh0218@gmail.com>
2017-07-24 20:57:06 +0900

    Fixed bug of searching a word which has no word class in the search result.

--- naver_endic_parse.py
+++ naver_endic_parse.py
@@ -1,20 +1,18 @@
 def naver_endic_parse(item):
     resulttext = '';
-    type = item.findAll('h3')[0].get_text('', strip=True)
-    body = item.findAll('dt', attrs={'class':'meanClass'})
-    resulttext += '<'+type+'>\n'
-    #if len(head) >= 1:
-    #    resulttext += head[0].get_text('', strip=True)
-    #    resulttext += '\n'
-    if len(body) >= 1:
-        for meaning in body:
-            mean = meaning.findAll('em')
-            for li in mean:
-                span = li.findAll('span')
-                for s in span:
-                    classes = dict(s.attrs)['class']
-                    if classes != ['blind']:
-                        resulttext += s.get_text('', strip=True)
-        resulttext += '\n'           
+    if len(item.findAll('h3')) > 0 :
+        #There wass no word class in the search result, the skip    
+        type = item.findAll('h3')[0].get_text('', strip=True)
+        resulttext += '<'+type+'>\n'
+    body = item.findAll('dt', attrs={'class':'meanClass'})    
+    for meaning in body:
+        mean = meaning.findAll('em')
+        for li in mean:
+            span = li.findAll('span')
+            for s in span:
+                classes = dict(s.attrs)['class']
+                if classes != ['blind']:
+                    resulttext += s.get_text('', strip=True)
+            resulttext += '\n'   
     return resulttext
     
